d74386a2770be5c898de4ed9dca2cf4d12cc93fc
almusallamrashed-create <almusallamrashed@gmail.com>
2026-03-29 07:55:45 +0300

    Update ElderTicket.java

--- ElderTicket.java
+++ ElderTicket.java
@@ -1,20 +1,20 @@
-package pckg;
+package Project_phase1;
 
 public class ElderTicket extends NormalTicket implements Discounts{
 private int age;
-public ElderTicket(double distance,String id, int age) {
-	super(distance,id);
+public ElderTicket(double travelDistance, String ticketNumber, String bookingDate, int age) {
+	super(travelDistance, ticketNumber, bookingDate);
 	this.age = age;
 }
 public double rideCost() {
-	return Math.ceil( distance*4.5*applyDiscount()*100) / 100.0;
+	return Math.ceil( travelDistance*4.5*applyDiscount()*100) / 100.0;
 }
 public double applyDiscount() {
 	return (double) 50/age;
 }
 @Override
 public Ticket copy() {
-	return new ElderTicket(this.getDistance(),this.getId(),this.getAge());
+	return new ElderTicket(this.getDistance(), this.getId(), this.getBookingDate(), this.getAge());
 }
 public String toString() {
 	String s = "Elder ";
