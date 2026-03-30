30382d5f790a0627f7f42ff7e77edaa56e01255b
almusallamrashed-create <almusallamrashed@gmail.com>
2026-03-30 23:40:38 +0300

    Update ElderTicket.java

--- ElderTicket.java
+++ ElderTicket.java
@@ -1,30 +1,39 @@
 package Project_phase1;
 
-public class ElderTicket extends NormalTicket implements Discounts{
-private int age;
-public ElderTicket(double travelDistance, String ticketNumber, String bookingDate, int age) {
-	super(travelDistance, ticketNumber, bookingDate);
-	this.age = age;
-}
-public double rideCost() {
-	return Math.ceil( travelDistance*4.5*applyDiscount()*100) / 100.0;
-}
-public double applyDiscount() {
-	return (double) 50/age;
-}
-@Override
-public Ticket copy() {
-	return new ElderTicket(this.getDistance(), this.getId(), this.getBookingDate(), this.getAge());
-}
-public String toString() {
-	String s = "Elder ";
-	s+= super.toString();
-	return s;
-}
-public int getAge() {
-	return age;
-}
-public void setAge(int age) {
-	this.age = age;
-}
+// ticket for older people with age discount
+public class ElderTicket extends NormalTicket implements Discounts {
+    private int age;
+    
+    public ElderTicket(double travelDistance, String ticketNumber, String bookingDate, int age) {
+        super(travelDistance, ticketNumber, bookingDate);
+        this.age = age;
+    }
+    
+    // calculate cost and round it
+    public double rideCost() {
+        return Math.ceil(travelDistance * 4.5 * applyDiscount() * 100) / 100.0;
+    }
+    
+    // discount formula based on age
+    public double applyDiscount() {
+        return (double) 50 / age;
+    }
+    
+    @Override
+    public Ticket copy() {
+        return new ElderTicket(this.getDistance(), this.getId(), this.getBookingDate(), this.getAge());
+    }
+    
+    public String toString() {
+        String s = "Elder ";
+        s += super.toString();
+        return s;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
