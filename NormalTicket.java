63929587bcf4950946d4e54d79f22005a9d87f0d
almusallamrashed-create <almusallamrashed@gmail.com>
2026-03-30 21:45:57 +0300

    Update NormalTicket.java

--- NormalTicket.java
+++ NormalTicket.java
@@ -1,18 +1,18 @@
-package pckg;
+package Project_phase1;
 
 public class NormalTicket extends Ticket {
-public NormalTicket(double distance,String id) {
-	super(distance,id);
+public NormalTicket(double travelDistance,String ticketNumber,String bookingDate) {
+	super(travelDistance,ticketNumber,bookingDate);
 }
 public double rideCost() {
-	return distance*5;
+	return travelDistance*5;
 }
 @Override
 public Ticket copy() {
-	return new NormalTicket(this.getDistance(),this.getId());
+	return new NormalTicket(this.getDistance(),this.getId(),this.getBookingDate());
 }
 public String toString() {
-	String s = "Ticket " + id + "\n";
+	String s = "Ticket " + ticketNumber + "\n";
 	s+= super.toString();
 	return s;
 }
