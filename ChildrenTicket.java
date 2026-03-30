0b65941bc34466375615f7df0fe3a885a56f701b
almusallamrashed-create <almusallamrashed@gmail.com>
2026-03-30 23:41:07 +0300

    Update ChildrenTicket.java

--- ChildrenTicket.java
+++ ChildrenTicket.java
@@ -1,20 +1,25 @@
 package Project_phase1;
 
+// ticket for kids
 public class ChildrenTicket extends NormalTicket {
-	public ChildrenTicket(double travelDistance, String ticketNumber, String bookingDate) {
-		super(travelDistance, ticketNumber, bookingDate);
-	}
-	public double rideCost() {
-		return travelDistance * 3;		
-	}
-	
-	@Override
-	public Ticket copy() {
-		return new ChildrenTicket(this.getDistance(), this.getId(), this.getBookingDate());
-	}
-	public String toString() {
-		String s = "Children ";
-		s += super.toString();
-		return s;
-	}
+    
+    public ChildrenTicket(double travelDistance, String ticketNumber, String bookingDate) {
+        super(travelDistance, ticketNumber, bookingDate);
+    }
+    
+    // cost is cheaper for children
+    public double rideCost() {
+        return travelDistance * 3;        
+    }
+    
+    @Override
+    public Ticket copy() {
+        return new ChildrenTicket(this.getDistance(), this.getId(), this.getBookingDate());
+    }
+    
+    public String toString() {
+        String s = "Children ";
+        s += super.toString();
+        return s;
+    }
 }
